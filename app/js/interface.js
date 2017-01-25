ff7a7e785ec6b6b00516668b6c9b181f32a08cde
Pasha-nineseven <pavel@nineseven.ru>
2017-01-25 18:55:21 +0200

    index(not-full)

--- app/js/interface.js
+++ app/js/interface.js
@@ -2,7 +2,18 @@ $(document).ready(function() {
 	// $("body").on("click", ".test", function(e){
 	// 	e.preventDefault();
 	// })
+	var $svg = $('.svg-anim').drawsvg();
+	$svg.drawsvg('animate');
 
+	if ($('.main-featured__slider').length>0) {
+		$('.main-featured__slider').slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			dots:false,
+			useTransform:true,
+		});
+	};
 });
 
 
@@ -20,16 +31,16 @@ $(window).resize(function () {
 
 
 // links pages
-$('body').append(
-	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-	<style> \
-		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
-		#pages a { text-decoration: none; } \
-		#pages li { margin: 5px 0; } \
-	</style> \
-	<ol id="pages"> \
-		<li><a href="about.html">About</a></li> \
-		<li><a href="index.html">Index</a></li> \
-	</ol> \
-</div>');
+// $('body').append(
+// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+// 	<style> \
+// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+// 		#pages a { text-decoration: none; } \
+// 		#pages li { margin: 5px 0; } \
+// 	</style> \
+// 	<ol id="pages"> \
+// 		<li><a href="about.html">About</a></li> \
+// 		<li><a href="index.html">Index</a></li> \
+// 	</ol> \
+// </div>');
